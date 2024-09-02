0c0e2d18b3e33d6322a2b7bb920da7bfb2930cc8
Kieran Llarena <baymax709@gmail.com>
2024-09-02 15:08:07 -0400

    feat: listen on port 8080. return 'Hello World!'

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,12 +3,21 @@
  */
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@SpringBootApplication
+
 public class App {
-    public String getGreeting() {
+    @GetMapping("/")
+    String home() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        SpringApplication.run(App.class, args);
     }
 }
